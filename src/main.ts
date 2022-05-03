bad68f7bbf32abb08a93438210cd520e9947ea46
tony dugué <duguetony@gmail.com>
2022-05-03 02:54:20 +0200

    add modules (auth, user & bookmark)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule); // on instancie l'application
+  await app.listen(3000); // on exécute le serveur sur le port 3000
 }
 bootstrap();
