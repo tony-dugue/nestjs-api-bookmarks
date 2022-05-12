38d2fa3ae61e069ab2263c8d5db29f75db5df877
tony dugué <duguetony@gmail.com>
2022-05-12 20:28:36 +0200

    add JWT strategy for signing token

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,16 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
