b84fbde8e5e9c528e930a5ed84329685ba3ea1bf
tony dugué <duguetony@gmail.com>
2022-05-14 01:13:50 +0200

    get user informations in response of the request /users/me

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
   @UseGuards(AuthGuard('jwt'))
   @Get('me') // /users/me
-  getMe() {
-    return 'user info';
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
+
+
+
+
