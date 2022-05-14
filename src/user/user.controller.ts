60da6cd820646e648b78ec84e04570524a44d0b4
tony dugué <duguetony@gmail.com>
2022-05-14 11:33:47 +0200

    get user informations with a protected route with token (+ protect with Nestjs Guards & optimization with custom decorator)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
-  @Get('me') // /users/me
-  getMe(@Req() req: Request) {
-    return req.user;
+  @Get('me') // route /users/me
+  getMe(@GetUser() user: User) {
+    return user;
   }
+
+  @Patch()
+  editUser() {};
 }
 
 
